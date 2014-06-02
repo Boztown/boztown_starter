960514d2d16b42ab075dc2feb71b4fc7806a1c80
Ryan Bosinger <rbosinger@gmail.com>
2014-06-02 11:22:52 -0600

    Added additional settings for analytics.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ BoztownStarter::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  GA.tracker = "xx-xxxxx-xx"
+
 end
