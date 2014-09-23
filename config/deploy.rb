dff0fb315b585ff61f71e664a8c4c2a88abb4366
Ryan Bosinger <rbosinger@gmail.com>
2014-09-23 16:44:34 +0000

    Added public/uploads to :linked_dirs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :rbenv_roles, :all # default value
 #set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/rbenv/shims:$PATH" }
