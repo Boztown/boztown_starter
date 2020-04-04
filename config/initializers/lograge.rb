e6e2534c15bc2c85562f3af3edcbaf6002b2b1ac
Ryan Bosinger <rbosinger@appcolony.ca>
2020-04-03 22:43:49 -0700

    Add Lograge and LogStash format logging config

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,10 @@
+Rails.application.configure do
+  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+
+  config.lograge.custom_payload do |controller|
+    {
+      user_id: controller.current_user.try(:id)
+    }
+  end
+end
