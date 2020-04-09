1f1efa63d93f2c0f98c489f93614ef9a8175edd3
Ryan Bosinger <rbosinger@appcolony.ca>
2020-04-09 16:04:55 -0700

    Switch <app> to <button> (don't need top level <app>)

---
+++ app/javascript/components/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  background: blue;
+  color: white;
+  font-size: 2em;
+  text-align: center;
+}
+</style>
